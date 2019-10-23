327b3e37bb70070273c30df6d70a82332dddaf35
Dave Syer <dsyer@pivotal.io>
2019-10-23 12:13:48 +0100

    Add prometheus

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,7 +1,9 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,4 +20,8 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Bean
+	public InMemoryHttpTraceRepository traceRepository() {
+		return new InMemoryHttpTraceRepository();
+	}
 }
