421b5f111d88ba0f1efd1dc07adedffd25dee5f7
Dave Syer <dsyer@pivotal.io>
2019-10-29 12:25:32 +0000

    Consolidate prometheus annotation automation
    If the annotation injector is running you don't need to declare the
    annotations. So there's no point splitting it up into 2 (3 would make
    sense - base+prom, annos, auto). Sticking with 1 for now.

--- demo/src/build/extra-libs.sh
+++ demo/src/build/extra-libs.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-TARGET=`dirname $0`/../../target
+BASE=`dirname $0`/../..
+TARGET=${BASE}/target
 
-if [ -z ${THIN_VERSION} ]; then THIN_VERSION=1.0.23.BUILD-SNAPSHOT; fi
+if [ -z ${THIN_VERSION} ]; then THIN_VERSION=1.0.23.RELEASE; fi
 if [ -z ${JAR_FILE} ]; then JAR_FILE=${TARGET}/docker-demo-0.0.1-SNAPSHOT.jar; fi
 THIN_JAR=~/.m2/repository/org/springframework/boot/experimental/spring-boot-thin-launcher/${THIN_VERSION}/spring-boot-thin-launcher-${THIN_VERSION}-exec.jar
 
-./mvnw dependency:get -Dartifact=org.springframework.boot.experimental:spring-boot-thin-launcher:${THIN_VERSION}:jar:exec -Dtransitive=false
+$BASE/mvnw dependency:get -Dartifact=org.springframework.boot.experimental:spring-boot-thin-launcher:${THIN_VERSION}:jar:exec -Dtransitive=false
 CPPARENT=`java -Dthin.trace=true -jar ${THIN_JAR} --thin.archive=${JAR_FILE} --thin.classpath`
 CPCHILD=`java -Dthin.trace=true -jar ${THIN_JAR} --thin.archive=${JAR_FILE} --thin.classpath --thin.parent=${JAR_FILE} --thin.profile=k8s`
 
