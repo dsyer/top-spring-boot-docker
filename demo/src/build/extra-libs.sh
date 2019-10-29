d6b075dccb62d310eff276cce56f1466de0393b3
Dave Syer <dsyer@pivotal.io>
2019-10-29 11:25:18 +0000

    Simplify extra-libs.sh a bit

--- demo/src/build/extra-libs.sh
+++ demo/src/build/extra-libs.sh
@@ -4,13 +4,13 @@ TARGET=`dirname $0`/../../target
 
 if [ -z ${THIN_VERSION} ]; then THIN_VERSION=1.0.23.BUILD-SNAPSHOT; fi
 if [ -z ${JAR_FILE} ]; then JAR_FILE=${TARGET}/docker-demo-0.0.1-SNAPSHOT.jar; fi
-M2_THIN_REPO=~/.m2/repository/org/springframework/boot/experimental/spring-boot-thin-launcher
+THIN_JAR=~/.m2/repository/org/springframework/boot/experimental/spring-boot-thin-launcher/${THIN_VERSION}/spring-boot-thin-launcher-${THIN_VERSION}-exec.jar
 
 ./mvnw dependency:get -Dartifact=org.springframework.boot.experimental:spring-boot-thin-launcher:${THIN_VERSION}:jar:exec -Dtransitive=false
-CPPARENT=`java -Dthin.trace=true -jar ${M2_THIN_REPO}/${THIN_VERSION}/spring-boot-thin-launcher-${THIN_VERSION}-exec.jar --thin.archive=${JAR_FILE} --thin.classpath`
-CPCHILD=`java -Dthin.trace=true -jar ${M2_THIN_REPO}/${THIN_VERSION}/spring-boot-thin-launcher-${THIN_VERSION}-exec.jar --thin.archive=${JAR_FILE} --thin.classpath --thin.parent=${JAR_FILE} --thin.profile=k8s`
+CPPARENT=`java -Dthin.trace=true -jar ${THIN_JAR} --thin.archive=${JAR_FILE} --thin.classpath`
+CPCHILD=`java -Dthin.trace=true -jar ${THIN_JAR} --thin.archive=${JAR_FILE} --thin.classpath --thin.parent=${JAR_FILE} --thin.profile=k8s`
 
 mkdir -p ${TARGET}/dependency/ext
-for f in `echo $CPCHILD | sed -e "s,$CPPARENT,," | tr ':' ' '`; do
+for f in `echo ${CPCHILD#${CPPARENT}*} | tr ':' ' '`; do
   cp $f ${TARGET}/dependency/ext;
 done
